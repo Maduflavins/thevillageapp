2c8342a5d779c7b1594c4b77821df52c5f9d6032
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-06-19 15:07:15 +0100

    added allowed host

--- introdesign/settings.py
+++ introdesign/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thevillageapp.herokuapp.com', 'localhost']
 
 
 # Application definition
