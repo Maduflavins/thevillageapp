43cacfec46e3a96438956b7b28baf73691d6ca6c
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-07-08 17:19:17 +0100

    changes

--- clock.py
+++ clock.py
@@ -1,45 +1,45 @@
-# from plans.models import Booking
-# # from django.core import mail
-# from django.utils import timezone
-# from apscheduler.schedulers.blocking import BlockingScheduler
+from plans.models import Booking
+# from django.core import mail
+from django.utils import timezone
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-# scheduler = BlockingScheduler()
-# @scheduler.scheduled_job('interval', seconds=60)
-# def toexpire():
-#     DT = timezone.now()
-#     bookings = Booking.objects.all()
-#     for booking in bookings:
-#         name = booking.firstname
-#         email = booking.email
-#         isExpired = booking.isExpired
-#         expiredDate = booking.expiresDate
-#         if DT > expiredDate:
-#             isExpired = True
-#             mail.send_mail(
-#                  'Expired',
-#                  name + ' plan has expired ',
-#                  'booking@village.ng',
-#                  ['maduabuchiokonkwo@gmail.com']
-#             )
+scheduler = BlockingScheduler()
+@scheduler.scheduled_job('interval', seconds=60)
+def toexpire():
+    DT = timezone.now()
+    bookings = Booking.objects.all()
+    for booking in bookings:
+        name = booking.firstname
+        email = booking.email
+        isExpired = booking.isExpired
+        expiredDate = booking.expiresDate
+        if DT > expiredDate:
+            isExpired = True
+            mail.send_mail(
+                 'Expired',
+                 name + ' plan has expired ',
+                 'booking@village.ng',
+                 ['maduabuchiokonkwo@gmail.com']
+            )
 
-# scheduler.start()
-# # scheduler.add_job(toexpire, 'interval', seconds=60)
-# # print("sending notifications")
-# # try:
+scheduler.start()
+# scheduler.add_job(toexpire, 'interval', seconds=60)
+# print("sending notifications")
+# try:
     
-# # except:
-# #     print(error)
+# except:
+#     print(error)
 
    
-# # from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 
-# # # sched = BlockingScheduler()
+# # sched = BlockingScheduler()
 
-# # @sched.scheduled_job('interval', minutes=3)
-# # def timed_job():
-# #     print('This job is run every three minutes.')
-# # sched.start()
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
+# sched.start()
 
 
 
