7c8cb478784f2791ca15b1d2e524763f30790aa4
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-07-08 04:32:58 +0100

    fixed clock.py errors

--- clock.py
+++ clock.py
@@ -1,40 +1,49 @@
-from plans.models import Booking
-from django.core import mail
-import datetime as DT
+# from plans.models import Booking
+# from django.core import mail
+# import datetime as DT
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def notify_user():
-    bookings = Booking.objects.all()
-    now = DT.datetime.now()
-    expire_date = bookings.expiresDate
-    support = 'booking@village.ng'
-    if DT.datetime.now() >= expire_date:
-        for booking in bookings:
-            name = booking.firstname + " " + booking.lastname
-            email = booking.email
-            phone = booking.phone
-            plan = booking.plan
-            message1 = (
-                    'Booking Expired',
-                    'Dear ' + name + ' your plan, ' + plan + ' with us at thevillage just expired. You can reach us immediately at 07089996339 for to renew',
-                    'booking@village.ng',
-                    [email]
-                )
+# @sched.scheduled_job('interval', minutes=1)
+# def notify_user():
+#     bookings = Booking.objects.all()
+#     now = DT.datetime.now()
+#     expire_date = bookings.expiresDate
+#     support = 'booking@village.ng'
+#     if DT.datetime.now() >= expire_date:
+#         for booking in bookings:
+#             name = booking.firstname + " " + booking.lastname
+#             email = booking.email
+#             phone = booking.phone
+#             plan = booking.plan
+#             message1 = (
+#                     'Booking Expired',
+#                     'Dear ' + name + ' your plan, ' + plan + ' with us at thevillage just expired. You can reach us immediately at 07089996339 for to renew',
+#                     'booking@village.ng',
+#                     [email]
+#                 )
 
-            message2 = (
-                'Expired plan',
-                'Please call ' + name + ' at ' + phone + ' their plan ' + plan + ' has expired',
-                'booking@village.ng',
-                ['maduabuchiokonkwo@gmail.com']
-            )
-            mail.send_mass_mail((message1, message2), fail_silently=False)
+#             message2 = (
+#                 'Expired plan',
+#                 'Please call ' + name + ' at ' + phone + ' their plan ' + plan + ' has expired',
+#                 'booking@village.ng',
+#                 ['maduabuchiokonkwo@gmail.com']
+#             )
+#             mail.send_mass_mail((message1, message2), fail_silently=False)
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
 sched.start()
 
-            
+
+
 
         
 
