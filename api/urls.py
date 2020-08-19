4e036673ac858c4e4110e2eb996f5e0812f28497
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-08-19 12:33:00 +0100

    added authentication

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI,RequestPasswordResetEmail,SetNewPasswordAPI
+
+
+
+name="api"
+
+urlpatterns = [
+  path('register/', RegisterView.as_view(), name="register") ,
+  path('email-verify/',VerifyEmail.as_view(), name='email-verify' ),
+  path('login/',LoginAPIView.as_view(), name='login' ),
+  path('request-reset-email/', RequestPasswordResetEmail.as_view(),name="request-reset-email"),
+  path('password-reset/<uidb64>/<token>', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+  path('password-reset-complete/', SetNewPasswordAPI.as_view(), name='password-reset-complete')
+
+
+]
